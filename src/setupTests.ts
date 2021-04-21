b19e9f35ee71dd182cceaa953f80123f64d0fe69
Mikołaj Duchewicz <mikolaj.duchewicz@mug.pl>
2021-04-21 19:22:00 +0200

    up

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,5 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-import axios from "axios";
-axios.defaults.baseURL = "https://www.metaweather.com/api";
