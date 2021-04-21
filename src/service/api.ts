8e820b954f5e1cfb5e35789d0cc8d6f18a1714ef
Mikołaj Duchewicz <mikolaj.duchewicz@mug.pl>
2021-04-21 19:57:57 +0200

    fix cors problem

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,5 +4,8 @@ const baseURL = "https://www.metaweather.com/api";
 
 export const api = axios.create({
   baseURL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
   timeout: 30 * 1000,
 });
