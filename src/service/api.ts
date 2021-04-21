cab23a6b6733679b3a3da08f39a0e7f86b17dce6
Mikołaj Duchewicz <mikolaj.duchewicz@mug.pl>
2021-04-21 20:04:42 +0200

    up

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,8 +4,5 @@ const baseURL = "https://www.metaweather.com/api";
 
 export const api = axios.create({
   baseURL,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
   timeout: 30 * 1000,
 });
