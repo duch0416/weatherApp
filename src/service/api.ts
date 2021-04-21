81e216a825f7b00dee77422b7ce4cc47da59e99b
Mikołaj Duchewicz <mikolaj.duchewicz@mug.pl>
2021-04-21 20:18:34 +0200

    add content type header

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,5 +4,8 @@ const baseURL = "https://www.metaweather.com/api";
 
 export const api = axios.create({
   baseURL,
+  headers: {
+    'Content-Type': 'application/json'
+ },
   timeout: 30 * 1000,
 });
